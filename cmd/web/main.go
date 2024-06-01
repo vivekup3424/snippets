f2dc9c1a1f7b8d523e6edc73c6c1ed386308ceae
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-06-02 03:51:51 +0530

    linking the html with css & jsneed to act authorization so that user's cant see the static files on the fileserver handler

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,6 +9,14 @@ func main() {
 	// Register the two new handler functions and corresponding URL patterns with
 	// the servemux, in exactly the same way that we did before.
 	router := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// Use the mux.Handle() function to register the file server as the handler for
+	// all URL paths that start with "/static/". For matching paths, we strip the
+	// "/static" prefix before the request reaches the file server.
+	router.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	//Registering the routes
 	router.HandleFunc("GET /", home)
 	router.HandleFunc("GET /snippet/view", snippetView)
 	router.HandleFunc("GET /snippet/view/{id}", snippetViewById)
