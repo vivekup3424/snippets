0500e82d55c860002cdeb6ce77138ac90459e316
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-06-02 06:20:03 +0530

    add App for logging purposes, and inject the it onto the handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -6,12 +6,12 @@ import (
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *Application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		w.WriteHeader(http.StatusNotFound)
 		//http.NotFound(w, r)
 		w.Write([]byte("Error 404: Page not found"))
-		InfoLogger.Println(w.Header())
+		app.InfoLogger.Println(w.Header())
 		return
 	}
 	// Use the template.ParseFiles() function to read the template file into a
@@ -26,41 +26,38 @@ func home(w http.ResponseWriter, r *http.Request) {
 	// represents any dynamic data that we want to pass in, which for now we'll
 	// leave as nil.
 	if err != nil {
-		ErrorLogger.Println(err)
+		app.ErrorLogger.Println(err)
 		http.Error(w, "internal server error:- failure to parse html", http.StatusInternalServerError)
 		return
 	}
 	//render the parsed html pages on the endpoint
 	if err := ts.Execute(w, nil); err != nil {
-		ErrorLogger.Println(err)
+		app.ErrorLogger.Println(err)
 		http.Error(w, "Internal server error when rendering html pages", http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "text/plain")
-	w.WriteHeader(http.StatusOK)
-
 	//w.Write([]byte("Hello from Snippetbox\n"))
-	InfoLogger.Println(w.Header())
+	app.InfoLogger.Println(w.Header())
 }
 
 // Add a snippetView handler function.
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *Application) snippetView(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
-	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Display a specific snippet...\n"))
-	InfoLogger.Println(w.Header())
+	app.InfoLogger.Println(w.Header())
 }
-func snippetViewById(w http.ResponseWriter, r *http.Request) {
+func (app *Application) snippetViewById(w http.ResponseWriter, r *http.Request) {
 	id := r.PathValue("id")
 	//idNum := strconv.Atoi(id)
 	str := fmt.Sprintf("Displaying the snippets of user with id = %s\n", id)
 	w.Write([]byte(str))
-	InfoLogger.Println(w.Header())
+	app.InfoLogger.Println(w.Header())
 }
 
 // Add a snippetCreate handler function.
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *Application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	//I dont need this since Go 1.22 handles this itself
 	//if r.Method != "POST" {
 	// 	w.Header().Set("Allow","POST")
@@ -69,7 +66,6 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	//
 	//}
 	w.Header().Set("Content-Type", "text/plain")
-	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Create a new snippet...\n"))
-	InfoLogger.Println(w.Header())
+	app.InfoLogger.Println(w.Header())
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,21 +6,29 @@ import (
 	"os"
 )
 
-// Use log.New() to create a logger for writing information messages. This takes
-// three parameters: the destination to write the logs to (os.Stdout), a string
-// prefix for message (INFO followed by a tab), and flags to indicate what
-// additional information to include (local date and time). Note that the flags
-// are joined using the bitwise OR operator |.
-var InfoLogger = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-
-// Create a logger for writing error messages in the same way, but use stderr as
-// the destination and use the log.Lshortfile flag to include the relevant
-// file name and line number.
-var ErrorLogger = log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+type Application struct {
+	InfoLogger  *log.Logger
+	ErrorLogger *log.Logger
+}
 
 func main() {
+	// Use log.New() to create a logger for writing information messages. This takes
+	// three parameters: the destination to write the logs to (os.Stdout), a string
+	// prefix for message (INFO followed by a tab), and flags to indicate what
+	// additional information to include (local date and time). Note that the flags
+	// are joined using the bitwise OR operator |.
+	var InfoLogger = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	// Create a logger for writing error messages in the same way, but use stderr as
+	// the destination and use the log.Lshortfile flag to include the relevant
+	// file name and line number.
+	var ErrorLogger = log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 	// Register the two new handler functions and corresponding URL patterns with
 	// the servemux, in exactly the same way that we did before.
+	app := &Application{
+		InfoLogger:  InfoLogger,
+		ErrorLogger: ErrorLogger,
+	}
 	router := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
@@ -30,13 +38,19 @@ func main() {
 	router.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	//Registering the routes
-	router.HandleFunc("GET /", home)
-	router.HandleFunc("GET /snippet/view", snippetView)
-	router.HandleFunc("GET /snippet/view/{id}", snippetViewById)
-	router.HandleFunc("POST /snippet/create", snippetCreate)
-	InfoLogger.Println("Starting server on 127.0.0.1:4001")
+	router.HandleFunc("GET /", app.home)
+	router.HandleFunc("GET /snippet/view", app.snippetView)
+	router.HandleFunc("GET /snippet/view/{id}", app.snippetViewById)
+	router.HandleFunc("POST /snippet/create", app.snippetCreate)
+	//adding the server config
+	srv := &http.Server{
+		Addr:     "localhost:4001",
+		Handler:  router,
+		ErrorLog: ErrorLogger,
+	}
+	InfoLogger.Printf("Starting server on %s\n", srv.Addr)
 	//making the server listen on appropriate port
-	if err := http.ListenAndServe("localhost:4001", router); err != nil {
+	if err := srv.ListenAndServe(); err != nil {
 		ErrorLogger.Fatal(err)
 	}
 }
